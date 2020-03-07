ec5f876ed2620c473a96e5217ec53def74995db0
infernoyy <1085435176@qq.com>
2020-03-07 23:48:46 +0800

    first commit

---
+++ src/App.vue
@@ -0,0 +1,36 @@
+<template>
+	<div id="app">
+<!-- 		<img src="./assets/logo.png">
+		<div>
+			<el-button @click="startHacking">Start</el-button>
+		</div> -->
+		<radioVue></radioVue>
+	</div>
+</template>
+
+<script>
+	import radioVue from './components/radio.vue'
+
+	export default {
+		components: {
+			radioVue
+		},
+		methods: {
+			startHacking() {
+				this.$notify({
+					title: 'It works!',
+					type: 'success',
+					message: 'We\'ve laid the ground work for you. It\'s time for you to build something epic!',
+					duration: 5000
+				})
+			}
+		}
+	}
+</script>
+
+<style>
+	#app {
+		font-family: Helvetica, sans-serif;
+		text-align: center;
+	}
+</style>
---
+++ src/components/radio.vue
@@ -0,0 +1,29 @@
+<template>
+	<div>
+		<el-radio-group v-model="radio3" size="small" @change="valChange">
+		  <el-radio label="1" border>备选项1</el-radio>
+		  <el-radio label="2" border>备选项2</el-radio>
+		  <el-radio label="3" border :disabled="isDisabled">备选项3</el-radio>
+		</el-radio-group>
+	</div>
+</template>
+
+<script>
+  export default {
+    data () {
+      return {
+        radio3: '1',
+		isDisabled: false
+      };
+    },
+	methods:{
+		valChange(option){
+			console.log(option)
+			if(option==1){
+				console.log(this.isDisabled)
+				this.isDisabled=true
+			}
+		}
+	}
+  }
+</script>
